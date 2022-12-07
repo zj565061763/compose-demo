7e79edd0cd51bfa822630c21b204828f8adb84bb
zj565061763 <565061763@qq.com>
2022-12-07 23:17:49 +0800

    crash

--- app/src/main/java/com/sd/demo/compose_demo/MainActivity.kt
+++ app/src/main/java/com/sd/demo/compose_demo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        SubDisplay().setContent { }
         AppDisplay().setContent { }
     }
 }
