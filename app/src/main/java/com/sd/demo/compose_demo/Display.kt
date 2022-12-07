6b62e3db5ec8aef17224d073874fcabf37b61385
zj565061763 <565061763@qq.com>
2022-12-07 20:36:50 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_demo/Display.kt
+++ app/src/main/java/com/sd/demo/compose_demo/Display.kt
@@ -1,9 +1,12 @@
 package com.sd.demo.compose_demo
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 open class Display {
-    var content: @Composable () -> Unit = {}
+    var content by mutableStateOf<(@Composable () -> Unit)?>(null)
 }
 
 open class SubDisplay : Display()
